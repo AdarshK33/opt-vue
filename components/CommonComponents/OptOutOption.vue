ac16af29f69006d4541c9f692d0e77b7372270cf
adkumar <adarsh.kumar@bnedloudcloud.com>
2022-05-10 16:15:13 +0530

    BNCFDC-510 | opt out success

--- components/CommonComponents/OptOutOption.vue
+++ components/CommonComponents/OptOutOption.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="text-center optOutOption">
-    <h5 class="mt-40">Do you want to <span class="optOutSpan">opt out?</span></h5>
+    <h5 class="mt-40">
+      Do you want to <span class="optOutSpan">opt out?</span>
+    </h5>
 
     <h5>
       Opting out means you will be responsible for getting your required course
@@ -12,15 +14,17 @@
         Opt out for:
 
         <select name="Spring" id="spring" class="spring_dropdown mt-20">
-            <option value="one"> Spring 2022 </option>
-            <option value="two"> Spring 2022 </option>
-            <option value="three"> Spring 2022 </option>
-            <option value="four"> Spring 2022 </option>
-          </select>
+          <option value="one">Spring 2022</option>
+          <option value="two">Spring 2022</option>
+          <option value="three">Spring 2022</option>
+          <option value="four">Spring 2022</option>
+        </select>
       </p>
     </div>
     <div class="mt-20">
-      <button id="btnOptOut" role="button">Opt Out</button>
+      <button id="btnOptOut" role="button" @click="submitOptOutBtn()">
+        Opt Out
+      </button>
     </div>
     <div class="mt-40"></div>
   </div>
@@ -43,7 +47,13 @@ export default {
       title: '',
     }
   },
-
+  methods: {
+    submitOptOutBtn() {
+      this.$router.push({
+        name: 'opt-out-success',
+      })
+    },
+  },
 }
 </script>
 <style scoped>
@@ -54,38 +64,36 @@ export default {
   font-size: 15px;
   font-weight: 700;
   color: #000000;
-  padding:10px 0 10px;
+  padding: 10px 0 10px;
   border-color: white;
   border-bottom: 1px solid black;
   font-family: 'ProximaNovaBold';
 }
-.optOutOption h5
-{
+.optOutOption h5 {
   font-family: 'ProximaNovaBold';
   font-size: 15px;
 }
 .optOutOption button {
-    background-color: #00274c;
-    font-style: normal;
-    font-family:  'ProximaNova';
-    border-radius: 4px;
-    height: 35px;
-    color: #fff;
-    text-align: center;
-    vertical-align: middle;
-    line-height: 1.75em;
-    padding: 5px 2em;
-    font-size: 14px;
-    border: none;
-    font-weight: 400;
-    display: inline-block;
-    cursor: pointer;
-    transition: 0.3s;
-    width:220px;
-    text-transform: uppercase;
-  }
- .optOutSpan{
-
-    font-family:  'ProximaNova';
- }
+  background-color: #00274c;
+  font-style: normal;
+  font-family: 'ProximaNova';
+  border-radius: 4px;
+  height: 35px;
+  color: #fff;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 1.75em;
+  padding: 5px 2em;
+  font-size: 14px;
+  border: none;
+  font-weight: 400;
+  display: inline-block;
+  cursor: pointer;
+  transition: 0.3s;
+  width: 220px;
+  text-transform: uppercase;
+}
+.optOutSpan {
+  font-family: 'ProximaNova';
+}
 </style>
---
+++ pages/opt-out-success/index.vue
@@ -0,0 +1,25 @@
+<template>
+ <OptOutSuccessPage />
+</template>
+
+<script>
+import OptOutSuccessPage from '../../components/CommonComponents/OptOutSuccessPage.vue'
+export default {
+  name: 'index',
+  components: {
+    OptOutSuccessPage
+  },
+  data() { 
+    return {
+   
+    }
+  },
+  props: [],
+ 
+
+}
+</script>
+
+<style>
+
+</style>
